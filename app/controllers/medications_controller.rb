b6aaadc8c12440008dd8ac8906a83d33a64222a4
Charles Painter <cpainter85@gmail.com>
2015-03-10 13:33:23 -0600

    add dependent destroy

--- app/controllers/medications_controller.rb
+++ app/controllers/medications_controller.rb
@@ -2,4 +2,9 @@ class MedicationsController < ApplicationController
   def show
     @medication = Medication.find(params[:id])
   end
+
+  def destroy
+    medication = Medication.destroy(params[:id])
+    redirect_to root_path, notice: "Deleted Medication: #{medication.name}"
+  end
 end
--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -2,4 +2,9 @@ class PatientsController < ApplicationController
   def show
     @patient = Patient.find(params[:id])
   end
+
+  def destroy
+    patient = Patient.destroy(params[:id])
+    redirect_to root_path, notice: "Deleted Patient: #{patient.full_name}"
+  end
 end
